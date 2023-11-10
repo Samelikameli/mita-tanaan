b1d3b624f3c217a1ce50d9ce394c23b149b84de4
Kaappo Raivio <kaappo.raivio@gmail.com>
2023-11-10 22:05:30 +0200

    add frontend code

---
+++ front/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+type  Props = {
+    moi: string
+}
+
+const App = ({moi}: Props) => {
+    return (
+        <div>
+            Tästä tulee hyvä pöhinä
+        </div>
+    );
+};
+
+export default App
