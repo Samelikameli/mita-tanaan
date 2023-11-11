d337583d462b2cd5bdc12f2e114ea3a2bdb3c7cb
Kaappo Raivio <kaappo.raivio@gmail.com>
2023-11-12 00:12:38 +0200

    silent screen placeholder

--- front/src/ui/activities/SilentActivityView.tsx
+++ front/src/ui/activities/SilentActivityView.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Text } from "@chakra-ui/react";
+import Avatar from "../account/Avatar.tsx";
 
 const SilentActivityView = () => {
-    return <div>Silentness</div>;
+    return (
+        <div
+            style={{
+                background: "darkgray",
+                height: "100vh",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+            }}>
+            <Avatar animal={"dog"}></Avatar>
+            <Text style={{ fontWeight: "bold", fontSize: "90%", marginTop: "10px" }}>Have fun with your friends!</Text>
+        </div>
+    );
 };
 
 export default SilentActivityView;
