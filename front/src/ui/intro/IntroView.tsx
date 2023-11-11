dcb76110735073fa0246272a709568aea98d4049
Tuomas Karjalainen <11463784+tuomas-tk@users.noreply.github.com>
2023-11-11 12:23:59 +0200

    feat: animations to activity list

--- front/src/ui/intro/IntroView.tsx
+++ front/src/ui/intro/IntroView.tsx
@@ -2,6 +2,7 @@ import { Button, VStack } from "@chakra-ui/react";
 import UserContext from "../../usercontext";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
+import { motion } from "framer-motion";
 
 const IntroView = () => {
     const user = useContext(UserContext);
@@ -9,10 +10,14 @@ const IntroView = () => {
 
     return (
         <VStack justifyContent="center" textAlign="center" height="100%" background="background.100">
-            <h2 style={{ fontWeight: "bold" }}>What are we doing today, {user?.name}?</h2>
-            <Button colorScheme="blue" onClick={() => navigate("/activities")}>
-                Continue
-            </Button>
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+                <h2 style={{ fontWeight: "bold" }}>What are we doing today, {user?.name}?</h2>
+            </motion.div>
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.8, duration: 1 }}>
+                <Button colorScheme="blue" onClick={() => navigate("/activities")}>
+                    Continue
+                </Button>
+            </motion.div>
         </VStack>
     );
 };
