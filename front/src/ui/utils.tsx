29fa0d3bb3730bfaaf57bb5b5fe30f5a0da2af91
Kaappo Raivio <kaappo.raivio@gmail.com>
2023-11-12 05:45:57 +0200

    after junction

--- front/src/ui/utils.tsx
+++ front/src/ui/utils.tsx
@@ -1,5 +1,5 @@
 const timeModeToEmoji = (timeMode: string | null) => {
-    if (timeMode == "after-school") return { emoji: "🏫", name: "After school" };
+    if (timeMode == "after-school") return { emoji: "🚀", name: "After junction" };
     if (timeMode == "after-dinner") return { emoji: "🍽️", name: "After dinner" };
     if (timeMode == "custom") return { emoji: "🕒", name: "Custom time" };
     return { emoji: "❓", name: "UNKNOWN" };
